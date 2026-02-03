3ecd5e737a65b9397219ce222f1162d20059cdc5
GandhiRam2202 <gandhiramparthi.com>
2026-02-03 09:19:26 +0530

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,40 +1,31 @@
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { clearSession } from "../utils/storage";
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { clearSession } from '../utils/storage';
 
 const api = axios.create({
-  baseURL: "https://padips2back.onrender.com/auth",
+  baseURL: process.env.EXPO_PUBLIC_API_BASE_URL,
   timeout: 10000,
   headers: {
-    "x-api-key": "PADIPS2_SECERET_KEY",
+    'x-api-key': process.env.EXPO_PUBLIC_API_KEY,
   },
 });
 
-/* REQUEST */
 api.interceptors.request.use(async (config) => {
-  const token = await AsyncStorage.getItem("token");
-
+  const token = await AsyncStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
-/* RESPONSE */
 api.interceptors.response.use(
   (res) => res,
   async (err) => {
-    if (
-      err.response?.status === 403 &&
-      err.response?.data?.forceLogout
-    ) {
+    if (err.response?.status === 403 && err.response?.data?.forceLogout) {
       await clearSession();
     }
     return Promise.reject(err);
   }
 );
-
-
 
 export default api;
